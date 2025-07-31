d38ba0fd944a90b3bd672e35b39643fe2c22cbac
Kauã Vargas <kauakvbbinello@gmail.com>
2025-07-30 21:10:39 -0300

    Criei uma nova tela para região de Johto
    Novo html e js para uma nova tela da região de Johto.

--- Pokedex-master/scriptJohto.js
+++ Pokedex-master/scriptJohto.js
@@ -1,14 +1,76 @@
 document.addEventListener('DOMContentLoaded', () => {
     const pokedexContainer = document.querySelector('.pokedex-container');
 
+    //Chikorita, Cyndaquil e Totodile, além dos lendários Ho-Oh, Lugia, Raikou, Entei e Suicune.
+
     const pokemons = [
         {
-            id: 3,
-            name: 'Venusaur',
-            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png',
-            types: ['grass', 'poison']
-        } 
-    ];
+            "id": 152,
+            "name": "Chikorita",
+            "types": [
+                "grass"
+            ],
+            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/152.png"
+        },
+        {
+            "id": 155,
+            "name": "Cyndaquil",
+            "types": [
+                "fire"
+            ],
+            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/155.png"
+        },
+        {
+            "id": 158,
+            "name": "Totodile",
+            "types": [
+                "water"
+            ],
+            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/158.png"
+        },
+        {
+            "id": 243,
+            "name": "Raikou",
+            "types": [
+                "electric"
+            ],
+            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/243.png"
+        },
+        {
+            "id": 244,
+            "name": "Entei",
+            "types": [
+                "fire"
+            ],
+            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/244.png"
+        },
+        {
+            "id": 245,
+            "name": "Suicune",
+            "types": [
+                "water"
+            ],
+            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/245.png"
+        },
+        {
+            "id": 249,
+            "name": "Lugia",
+            "types": [
+                "psychic",
+                "flying"
+            ],
+            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/249.png"
+        },
+        {
+            "id": 250,
+            "name": "Ho-Oh",
+            "types": [
+                "fire",
+                "flying"
+            ],
+            "image": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/250.png"
+        }
+    ]
 
     const createPokemonCard = (pokemon) => {
         const card = document.createElement('div');
